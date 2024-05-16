dce6b293aba810f5d36bb194f11318d86303760b
João Gabriel Noleto de Figueiredo <jfigueiredo@sga.pucminas.br>
2024-05-16 20:02:12 -0300

    criei o json server para a guardar as receitas geradas

--- codigo/pag_ger_receita/genscript.js
+++ codigo/pag_ger_receita/genscript.js
@@ -1,5 +1,5 @@
 import { GoogleGenerativeAI } from 'https://esm.run/@google/generative-ai'
-
+const url = 'https://f733644a-cf2c-4aa0-9868-0fe63979ff2d-00-3hi4zrb19xfrs.spock.replit.dev/receitas'
 const genAI = new GoogleGenerativeAI("AIzaSyBxWCkyRq3c0jhycOo01gnIq15EaKjNA-c");
 
 async function run() {
@@ -11,9 +11,26 @@ async function run() {
 
   const result = await model.generateContent(prompt)
   const response = await result.response
+  const nreceita = {  
+    pedido: prompter,
+    "modo de preparo": response.text()
+  }
+  const opcoes = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(nreceita)
+  }
   const text = response.text()
   console.log("deu certo!")
   receita.textContent = text
+  fetch(url, opcoes)
+  .then(response => response.json())
+  .then(data => console.log(data)) // Exibe a resposta do servidor no console
+  .catch(error => console.error('Erro ao salvar a receita no histórico!', error))
 }
+
+
 let btn = document.getElementById("botao")
 btn.addEventListener("click", run)
